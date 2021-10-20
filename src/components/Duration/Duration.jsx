4d304422bf7625b02870d33b914932c1e3a5d33a
Julia Astakhova <walkaway1919@gmail.com>
2021-10-20 18:11:15 +0300

    fix paths

--- src/components/Duration/Duration.jsx
+++ src/components/Duration/Duration.jsx
@@ -4,11 +4,11 @@ export const Duration = ({ className, weather, msToTime }) => {
   return (
     <div className={cn(className, "length-day")}>
       <div className="length-day__sunrise sunrise">
-        <img className="length-day__icon" src="/free-icon-sunrise.png" />
+        <img className="length-day__icon" src="free-icon-sunrise.png" />
         <div>{msToTime(weather.sys.sunrise)}</div>
       </div>
       <div className="length-day__sunset sunset">
-        <img className="length-day__icon" src="/free-icon-sunset.png" />
+        <img className="length-day__icon" src="free-icon-sunset.png" />
         <div>{msToTime(weather.sys.sunset)}</div>
       </div>
     </div>
--- src/components/IconText/IconText.jsx
+++ src/components/IconText/IconText.jsx
@@ -3,14 +3,14 @@ import "./IconText.scss";
 
 const iconMap = {
   temperature: (
-    <img className="icon-block__icon" src="/free-icon-temperature.png" />
+    <img className="icon-block__icon" src="free-icon-temperature.png" />
   ),
   pressure: (
-    <img className="icon-block__icon" src="/free-icon-low-tide-4064379.png" />
+    <img className="icon-block__icon" src="free-icon-low-tide-4064379.png" />
   ),
   wind: <img className="icon-block__icon" src="free-icon-windy.png" />,
-  cloud: <img className="icon-block__icon" src="/free-icon-cloud.png" />,
-  humidity: <img className="icon-block__icon" src="/free-icon-humidity.png" />,
+  cloud: <img className="icon-block__icon" src="free-icon-cloud.png" />,
+  humidity: <img className="icon-block__icon" src="free-icon-humidity.png" />,
 };
 
 export const IconText = ({ className, size, text, icon }) => {
@@ -21,25 +21,3 @@ export const IconText = ({ className, size, text, icon }) => {
     </div>
   );
 };
-
-{
-  /* <div className='weather-box__wind wind'>
-    <img src="free-icon-windy.png"/>
-    <div className='wind__info'>ветер {Math.round(weather.wind.speed)} м/с, {Math.round((weather.wind.speed) * 5/9)}°c</div>
-</div>
-
-<div className='weather-box__clouds clouds'>
-    <img src='/free-icon-cloud.png'/>
-    <div className='clouds__info'>облачность {weather.clouds.all}%</div>
-</div>
-
-<div className='weather-box__humidity humidity'>
-    <img src="/free-icon-humidity.png"/>
-    <div className='humidity__info'>влажность {weather.main.humidity}%</div>
-</div> 
-
-<div className='forecast__temp'> 
-    <img src='/free-icon-temperature.png'></img>
-    <div>{Math.round(averageTemp)} °c</div>
-</div> */
-}
